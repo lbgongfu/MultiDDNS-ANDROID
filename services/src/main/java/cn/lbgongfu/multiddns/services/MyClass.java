562183e5ca7a745d028fcdb94360561317a39b3c
lbgongfu <lbgongfu@163.com>
2015-12-22 19:33:47 +0800

    测试

--- services/src/main/java/cn/lbgongfu/multiddns/services/MyClass.java
+++ services/src/main/java/cn/lbgongfu/multiddns/services/MyClass.java
@@ -1,5 +1,8 @@
 package cn.lbgongfu.multiddns.services;
 
 public class MyClass {
-    public static void test(){}
+    public static void test()
+    {
+        System.out.print("Hello World");
+    }
 }
